ed0a157aa64f5ef30fafa3c4647dcbd9ec4006ed
selvendranks <selvendranks@gmail.com>
2022-09-09 06:33:52 +0530

    used react dom portals to shift errormodel to the direct child of root

--- src/UI/ErrorModel.js
+++ src/UI/ErrorModel.js
@@ -1,17 +1,14 @@
 import Button from "./Button";
+import ReactDOM from "react-dom";
 import Card from "./Card";
-import classes from './ErrorModel.module.css'
+import classes from "./ErrorModel.module.css";
 
-const ErrorModel = (props) => {
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.clearError}></div>;
+};
 
-
-  
+const ModalOverlay = (props) => {
   return (
-    
-
-
-    <div>
-     <div className={classes.backdrop} onClick = {props.clearError}></div>
     <Card className={classes.modal}>
       <header className={classes.header}>
         <h2>{props.title}</h2>
@@ -19,13 +16,31 @@ const ErrorModel = (props) => {
       <div className={classes.content}>
         <p>{props.message}</p>
       </div>
-      
+
       <footer className={classes.actions}>
-        <Button onClick= {props.clearError}>Okay</Button>
+        <Button onClick={props.clearError}>Okay</Button>
       </footer>
     </Card>
-    </div>
   );
 };
 
-export default ErrorModel
+const ErrorModel = (props) => {
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop clearError={props.clearError}></Backdrop>,
+        document.querySelector("#backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          clearError={props.clearError}
+        ></ModalOverlay>,
+        document.querySelector('#overlay-root')
+      )}
+    </>
+  );
+};
+
+export default ErrorModel;
