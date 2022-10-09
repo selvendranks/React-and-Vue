8164b32be499acc7c17b310026376318ed8f31da
selvendranks <selvendranks@gmail.com>
2022-10-09 10:45:42 +0530

    useCallbackhook to specify same address for object

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from "react";
 
-import './App.css';
-import DemoOutput from './components/Demo/DemoOutput';
-import Button from './components/UI/Button/Button';
+import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
+import Button from "./components/UI/Button/Button";
 
 function App() {
-  const [showPara,setShowPara] = useState(false)
-  console.log('app')
-  const toggleParaHandler = () => {
-       setShowPara(!showPara);
-  }
+  const [showPara, setShowPara] = useState(false);
+  console.log("app");
+
+  const toggleParaHandler = useCallback(() => {
+    setShowPara((showPara)=>!showPara);
+  },[]);
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false}></DemoOutput>
+      <DemoOutput show={showPara}></DemoOutput>
       <Button onClick={toggleParaHandler}>Submit</Button>
     </div>
   );
