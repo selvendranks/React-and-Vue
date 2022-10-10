a4f0685deeca8957dfa637a7d4988f9cabf344a9
selvendranks <selvendranks@gmail.com>
2022-10-10 06:25:59 +0530

    understanding useCallback dependencies (Closures)

--- src/App.js
+++ src/App.js
@@ -6,17 +6,25 @@ import Button from "./components/UI/Button/Button";
 
 function App() {
   const [showPara, setShowPara] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
   console.log("app");
 
   const toggleParaHandler = useCallback(() => {
-    setShowPara((showPara)=>!showPara);
-  },[]);
+    if (allowToggle) {
+      setShowPara((showPara) => !showPara);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle((allowToggle) =>!allowToggle);
+  };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
       <DemoOutput show={showPara}></DemoOutput>
       <Button onClick={toggleParaHandler}>Submit</Button>
+      <Button onClick={allowToggleHandler}>Allow toggle</Button>
     </div>
   );
 }
