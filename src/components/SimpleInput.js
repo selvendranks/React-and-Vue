f7953f1dfed61b233617002c9c5dbfb93433ab97
selvendranks <selvendranks@gmail.com>
2022-11-07 07:56:56 +0530

    added ways to get form input values

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,32 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const nameRef = useRef();
+
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredName);
+    console.log(nameRef.current.value);
+    setEnteredName("")
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={nameInputHandler}
+          ref={nameRef}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
