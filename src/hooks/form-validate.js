6f3d34f8b2c2b2b6fb1f1874287f1013e79ebe2d
selvendranks <selvendranks@gmail.com>
2022-11-09 17:02:55 +0530

    modified stateHandling with useReducer hook

--- src/hooks/form-validate.js
+++ src/hooks/form-validate.js
@@ -1,29 +1,39 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
+
+const initialInputState = {inputValue : "", enteredInputTouched : false}
+
+const inputs = (state,action)=>{
+     if(action.type === 'inputTouched')
+      return { inputValue  : state.inputValue , enteredInputTouched : true}
+     if(action.type === 'enteredInput')
+      return {inputValue : action.inputValue , enteredInputTouched : state.enteredInputTouched}
+}
 
 const useValidate = (condition) => {
-  const [inputValue, setEnteredInput] = useState("");
-  const [enteredInputTouched, setEnteredInputTouched] = useState(false);
+  // const [inputValue, setEnteredInput] = useState("");
+  // const [enteredInputTouched, setEnteredInputTouched] = useState(false);
 
-  const enteredInputIsValid = condition(inputValue)
+  const [input,inputDispatcher] = useReducer(inputs,initialInputState)
+
+  const enteredInputIsValid = condition(input.inputValue)
 
   const inputBlurHandler = (event) => {
-    setEnteredInputTouched(true);
+    inputDispatcher({type: 'inputTouched'})
   };
 
   const inputHandler = (event) => {
-    setEnteredInput(event.target.value);
-    setEnteredInputTouched(true);
+    inputDispatcher({type : 'enteredInput' , inputValue : event.target.value})
+    inputDispatcher({type: 'inputTouched'})
   };
 
-  let inputIsValid = !enteredInputIsValid && enteredInputTouched;
- 
+  let inputIsValid = !enteredInputIsValid && input.enteredInputTouched;
 
   let inputClasses = inputIsValid
     ? "form-control invalid"
     : "form-control";
 
  return({
-    inputValue,
+    inputValue : input.inputValue,
     inputHandler,
     inputBlurHandler,
     inputClasses,
