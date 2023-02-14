34ef03578377a8daa116e8afcfd20a328f7d0846
selvendranks <selvendranks@gmail.com>
2023-02-14 16:56:27 +0530

    method calling via interpollation

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,15 +1,20 @@
 const app = Vue.createApp({
-    data(){
-      return{
-        courseGoal:[
-            {text: 'Wikipedia', link:'www.wikipedia.com'},
-            {text:'Google', link: 'www.google.com'}
-        ]
-      }
+  data() {
+    return {
+      courseGoal: [
+        { text: "Wikipedia", link: "www.wikipedia.com" },
+        { text: "Google", link: "www.google.com" },
+      ],
+    };
+  },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber > 0.5) {
+        return "can do";
+      } else return "can't do";
     },
-    methods:{
-         
-    }
-  });
+  },
+});
 
-app.mount('#user-goal');
+app.mount("#user-goal");
