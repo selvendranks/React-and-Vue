d1cbcf0b0db532d15a8c356c088e1a6ffe2a39c7
selvendranks <selvendranks@gmail.com>
2023-02-15 16:06:49 +0530

    working with event arguments

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -4,7 +4,14 @@ const app = Vue.createApp({
       counter: 0,
     };
   },
-
+  methods:{
+     addCounter(num){
+       this.counter+=num;
+     },
+     removeCounter(num){
+      this.counter-=num;
+     }
+  }
 });
 
 app.mount('#events');
