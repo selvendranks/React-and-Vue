5a9e9d4c4de9357770c0b6fc98ea75721f13bd89
selvendranks <selvendranks@gmail.com>
2023-02-15 16:22:40 +0530

    added native event object

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,12 +2,17 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name : ''
     };
   },
   methods:{
-     addCounter(num){
+     
+      setName(event,name){
+          this.name = event.target.value + name ;
+      },
+     addCovvunter(num){
        this.counter+=num;
-     },
+     }, 
      removeCounter(num){
       this.counter-=num;
      }
