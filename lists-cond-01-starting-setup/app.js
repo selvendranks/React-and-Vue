5becd354c2521573ebffba7324cee6f82bca3a9f
selvendranks <selvendranks@gmail.com>
2023-02-18 17:02:44 +0530

    adding unique :key for v-for

--- lists-cond-01-starting-setup/app.js
+++ lists-cond-01-starting-setup/app.js
@@ -9,6 +9,9 @@ const app = Vue.createApp({
     addGoal(){
        this.goals.push(this.enteredGoal)
        this.enteredGoal = ''
+    },
+    removeGoal(index){
+       this.goals.splice(index,1)
     }
   }
 });
