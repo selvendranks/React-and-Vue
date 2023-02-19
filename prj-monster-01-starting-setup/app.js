47608d97f30179d291db13f52f919aba764f568b
selvendranks <selvendranks@gmail.com>
2023-02-19 13:58:59 +0530

    added special attack functionality

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -6,8 +6,16 @@ const app = Vue.createApp({
     data(){
         return{
             monsterHealth: 100,
-            playerHealth:100
+            playerHealth:100,
+            specialAttackReady:0
         }
+    },
+    watcher:{
+       specialAttackReady(){
+            if(this.specialAttackReady>3){
+                this.specialAtttackReady = 3;
+            }
+       }
     },
     computed:{
         monsterHealthBar(){
@@ -19,15 +27,24 @@ const app = Vue.createApp({
             return {
                 width: this.playerHealth + '%'
             }
+        },
+        mayUseSpecialAttack(){
+            return this.specialAttackReady < 3 ? true : false
         }
+        
     },
     methods:{
         attackMonster(){
           this.monsterHealth-= getRandomValue(12,5);
+          this.specialAttackReady+=1;
           this.attackPlayer();
         },
         attackPlayer(){
             this.playerHealth-= getRandomValue(18,8);
+        },
+        specialAttack(){
+            this.monsterHealth-= getRandomValue(25,10);
+            this.specialAttackReady = 0;
         }
     }
 })
