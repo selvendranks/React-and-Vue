ba28912493e59337dcceffdbe8b944c8630ce16c
selvendranks <selvendranks@gmail.com>
2023-02-25 16:55:15 +0530

    added provide and inject

---
+++ cmp-communication-08-a-potential-problem-starting-setup/src/components/KnowledgeBase.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+    <h2>Select a Topic</h2>
+    <knowledge-grid  @select-topic="$emit('select-topic', $event)"></knowledge-grid>
+  </section>
+</template>
+
+<script>
+export default {
+  emits: ['select-topic'],
+};
+</script>
