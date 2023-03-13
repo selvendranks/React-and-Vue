49d124762ba839d714ea6c2bb85b11919a16e2a9
selvendranks <selvendranks@gmail.com>
2023-03-13 21:09:29 +0530

    added error modal

--- cmp-adv-01-starting-setup/src/components/DynamicCmp1.vue
+++ cmp-adv-01-starting-setup/src/components/DynamicCmp1.vue
@@ -1,6 +1,43 @@
 <template>
     <div>
+        <error-alert v-if="error.iserror === true" >
+            <template #default>
+                <h2>{{error.errordesc}}</h2>
+                <button @click="confirmError()">Okay</button>
+            </template>
+        </error-alert>
         <h2>component1</h2>
-        <input type="text">
+        <label for="goalinput">Goal</label>
+        <input type="text" ref="goal" id="goalinput">
+        <button @click="addGoal()">Submit</button>
     </div>
 </template>
+
+<script>
+import errorAlert from './ErrorAlert.vue';
+export default {
+    components:{
+        'error-alert':errorAlert
+    },
+    data(){
+        return{
+            error:{
+                iserror:false,
+                errordesc:''
+            }
+        }
+    },
+    methods:{
+        addGoal(){
+            if(this.$refs.goal.value === ''){
+                this.error.iserror = true
+                this.error.errordesc = 'empty input field'
+            }
+        }
+        ,
+        confirmError(){
+            this.error.iserror = false;
+        }
+    }
+}
+</script>
---
+++ cmp-adv-01-starting-setup/src/components/ErrorAlert.vue
@@ -0,0 +1,25 @@
+<template>
+  <dialog open>
+    <slot></slot>
+    
+  </dialog>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
+
+<style scoped>
+ dialog{
+    background: white;
+    position: fixed;
+    top: 20vh;
+    left: 30%;
+    width: 40%;
+    box-shadow: 0 2px 8px gray;
+    padding: 1rem;
+    margin:0;
+ }
+</style>
