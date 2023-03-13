c1e8039f4b709b133b01c95dc5a7fa67c4693b46
selvendranks <selvendranks@gmail.com>
2023-03-13 18:33:02 +0530

    added dynamic components

--- cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+
+    <button @click="setcomponent('dynamiccmp-1')">Load component 1</button>
+    <button @click="setcomponent('dynamiccmp-2')">Load component 2</button>
+    <component :is="activeDynamicCmp"></component>
     <the-header></the-header>
     <badge-list></badge-list>
     <user-info
@@ -22,16 +26,22 @@ import TheHeader from "./components/TheHeader.vue";
 import BadgeList from "./components/BadgeList.vue";
 import UserInfo from "./components/UserInfo.vue";
 import scopedSlot from "./components/scopedSlot.vue";
+import DynamicCmp1 from "./components/DynamicCmp1.vue"
+import DynamicCmp2 from "./components/DynamicCmp2.vue"
 
 export default {
   components: {
+    
     "the-header": TheHeader,
     "badge-list": BadgeList,
     "user-info": UserInfo,
     "scoped-slot": scopedSlot,
+    "dynamiccmp-1":DynamicCmp1,
+    "dynamiccmp-2":DynamicCmp2
   },
   data() {
     return {
+      activeDynamicCmp : 'Default',
       activeUser: {
         name: "Maximilian Schwarzmüller",
         description: "Site owner and admin",
@@ -39,6 +49,11 @@ export default {
       },
     };
   },
+  methods:{
+    setcomponent(cmp){
+        this.activeDynamicCmp = cmp
+    }
+  }
 };
 </script>
 
